6c9ea3cd8a077c4adc0a08dbf812dbc1887e0f07
z3nn13 <paulphyo25@gmail.com>
2023-07-09 11:46:51 +0630

    feat(breeze): installed breeze)

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
-            valetTls: 'ngapaliparadisehotel.test',
         }),
     ],
 });
