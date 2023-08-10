8a40c704208e944a63e7df8f3390917edb8d99ec
z3nn13 <paulphyo25@gmail.com>
2023-08-10 22:23:10 +0630

    refactor(footer): used svgs instead of fontawesome for social icons

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,21 +1,41 @@
-<footer class="footer set-bg" data-set-bg="{{ asset('/images/home/footer.jpg') }}">
+<footer class="footer set-bg"
+    data-set-bg="{{ asset('/images/home/footer.jpg') }}">
     <div class="footer__wrapper">
-        <img class=" footer__logo" src="/images/logos/white_text.png" alt="logo">
+        <img class=" footer__logo"
+            src="/images/logos/white_text.png"
+            alt="logo">
         <div class="footer__details">
             <span class="footer__span">Ngapali Main Rd,<br>Thandwe, Myanmar 1221</span>
             <span class="footer__span">(+95) 951-1663</span>
             <span class="footer__span">ngapaliparadisehotel@gmail.com</span>
         </div>
         <ul class="footer__socials">
-            <li class="footer__icon"><i class="fa-brands fa-square-facebook"></i></li>
-            <li class="footer__icon"><i class="fa-brands fa-instagram"></i></li>
-            <li class="footer__icon"><i class="fa-brands fa-twitter"></i></li>
+            <li class="footer__icon">
+                <a href="https://www.facebook.com/ngapaliparadisehotel/">
+                    <img src="{{ asset('images/svgs/facebook-icon.svg') }}"
+                        alt="Facebook Icon">
+                </a>
+            </li>
+            <li class="footer__icon">
+                <a href="https://www.instagram.com/ngapaliparadise">
+                    <img src="{{ asset('images/svgs/instagram-icon.svg') }}"
+                        alt="Instagram Icon">
+                </a>
+            </li>
+            <li class="footer__icon">
+                <a href="https://www.twitter.com/ngapaliparadise">
+                    <img src="{{ asset('images/svgs/twitter-icon.svg') }}"
+                        alt="Twitter Icon">
+                </a>
+            </li>
         </ul>
         <nav class="footer__nav nav">
-            <li class="nav__item"><a class="nav__link" href="">rooms</a></li>
-            <li class="nav__item"><a class="nav__link" href="">contact</a></li>
-            <li class="nav__item"><a class="nav__link" href="">about</a></li>
-            <li class="nav__item"><a class="nav__link" href="">gallery</a></li>
+            <li class="nav__item"><a class="nav__link"
+                    href="">rooms</a></li>
+            <li class="nav__item"><a class="nav__link"
+                    href="">gallery</a></li>
+            <li class="nav__item"><a class="nav__link"
+                    href="">about</a></li>
         </nav>
         <span class="footer__copyright">© Copyright 2022 all rights reserved</span>
     </div>
